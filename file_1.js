3af05f23e8cbba6be923e380d6206d31bfb143cd
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-12-01 13:00:00 -0300

    Update file_1.js Criada a função 'calcularSubtracao'

--- file_1.js
+++ file_1.js
@@ -1,5 +1,7 @@
 const calcularDivisao = (_numA, _numB) => _numA / _numB
 const calcularSoma = (_numA, _numB) => _numA + _numB
+const calcularSubtracao  = (_numA, _numB) => _numA - _numB
 
 console.log(calcularDivisao(7, 4))
 console.log(calcularSoma(7, 9));
+console.log(`A subtração entre ${4} e ${7} é igual a ${calcularSubtracao(4, 7)}`)
