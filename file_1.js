589654007a3476c2e575e9693485888a64d7a4e0
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-12-01 12:55:01 -0300

    Update: file_1.js Criada a função 'calcularMultiplicacao' Atualizado a forma como os dados são mostrados na tela

--- file_1.js
+++ file_1.js
@@ -1,5 +1,7 @@
 const calcularDivisao = (_numA, _numB) => _numA / _numB
+const calcularMultiplicacao = (_numA, _numB) => _numA * _numB
 const calcularSoma = (_numA, _numB) => _numA + _numB
 
-console.log(calcularDivisao(7, 4))
-console.log(calcularSoma(7, 9));
+console.log(`A divisão entre ${7} e ${4} é igual a ${calcularDivisao(7, 4)}`)
+console.log(`A multiplicação entre ${3} e ${6} é igual a ${calcularMultiplicacao(3, 6)}`)
+console.log(`A soma entre ${7} e ${9} é igual a ${calcularSoma(7, 9)}`)
